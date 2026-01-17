73c4c76c90d31f0e50e0895a1e2237db7df809b1
Aqsa Nazir <aqsanazir39@gmail.com>
2026-01-17 16:46:40 +0500

    new code

---
+++ hoisting.jsx
@@ -0,0 +1,8 @@
+console.log(firstName);
+
+var firstName = "aqsa";
+let lastName = "Nazir";
+
+let age = 24;
+const yearOfAge = 2001;
+console.log(firstName);
--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Hello World!!!");
+let x = 9007199254740992n;
+let y = 9007199254740993n;
+
+console.log(x === y);
+
+let t = BigInt(145678907634223445);
+let p = BigInt(145678907634223445);
+
+console.log(t === p);
+
+let userName = "Aqsa";
+
+console.log(userName);
+
+let fullName = "My Name is Aqsa Nazir."; //camel case
+console.log(fullName);
+
+let dul_dul = "Nabi,s Kada name is duldul"; // snake case
+
+isHappay = true;
+console.log(isHappay);
+let age = 24;
+
+console.log(`${fullName} I am ${age}`);
+
+const hoursInADay = 24;
+// Const is used it is better to use.
