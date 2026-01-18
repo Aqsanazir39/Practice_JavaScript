abe80e92d57decf445d55c24e74a23c5b0c33579
Aqsa Nazir <aqsanazir39@gmail.com>
2026-01-18 10:44:28 +0500

    Learn Dialog boxes.

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-console.log("Hello World!!!");
-let x = 9007199254740992n;
-let y = 9007199254740993n;
+// // const isAlert = alert("Are you sure.");
+// const isConfirmed = confirm('Would you like to stay?');
+// const isPrompt  = prompt('Give me the your Favourite color name? ')
 
-console.log(x === y);
-
-let t = BigInt(145678907634223445);
-let p = BigInt(145678907634223445);
-
-console.log(t === p);
-
-let userName = "Aqsa";
-
-console.log(userName);
-
-let fullName = "My Name is Aqsa Nazir."; //camel case
-console.log(fullName);
-
-let dul_dul = "Nabi,s Kada name is duldul"; // snake case
-
-isHappay = true;
-console.log(isHappay);
-let age = 24;
-
-console.log(`${fullName} I am ${age}`);
-
-const hoursInADay = 24;
-// Const is used it is better to use.
+let namee = prompt("Enter your name?");
+alert("Your name is " + namee);
